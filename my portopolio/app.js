9b90ba8395893df482022c83a8e16ae1a699d372
prayoga475 <162825435+prayoga475@users.noreply.github.com>
2024-11-15 09:37:33 +0700

    Add files via upload

---
+++ my portopolio/app.js
@@ -0,0 +1,21 @@
+const toTop= document.querySelector(".back-top")
+
+window.addEventListener("scroll", () => {
+    if(pageYOffset > 75){
+        toTop.classList.add("active")
+    }else{
+        toTop.classList.remove("active")
+    }
+})
+
+const sidebar= document.querySelector(".links")
+const links= document.querySelectorAll(".links li a")
+
+
+links.forEach(link => {
+    link.addEventListener("click", toggleSidebar)
+})
+
+function toggleSidebar(){
+    sidebar.classList.toggle("show")
+}
